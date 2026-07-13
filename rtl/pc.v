3f5218af4199632a5f22723b31c7bebdaffdf595
Jerry-1360 <zyjerryj@gmail.com>
2026-07-13 03:04:04 -0400

    ALU, PC, reg file riv 1

---
+++ rtl/pc.v
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2026 Zhiyuan (Jerry) Jiang
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+ module pc (
+    input clk,
+    input rst_n,
+    input [31:0] pc_in,
+    output reg [31:0] pc_out
+ );
+
+ always @(posedge clk or negedge rst_n) begin
+
+    if (!rst_n) begin
+        pc_out <= 32'h0000_0000;
+    end
+
+    else begin
+        pc_out <= pc_in;
+    end
+    
+ end
+
+ 
+
+ endmodule
---
+++ rtl/reg_file.v
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2026 Zhiyuan (Jerry) Jiang
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+ module reg_file (
+    input clk,
+    input rst_n,
+    input [4:0] rs1_address,
+    input [4:0] rs2_address,
+    input [4:0] rd_address,
+    input [31:0] write_data,
+    input reg_write,
+    output [31:0] rs1_data,
+    output [31:0] rs2_data
+ );
+
+ reg [31:0] internal_reg [0:31];
+
+ assign rs1_data = (rs1_address == 0) ? 32'h0000_0000 : internal_reg[rs1_address];
+ assign rs2_data = (rs2_address == 0) ? 32'h0000_0000 : internal_reg[rs2_address];
+
+ integer i;
+
+ always @(posedge clk or negedge rst_n) begin
+
+    if (!rst_n) begin
+
+        for (i = 0; i < 32; i = i + 1) begin
+            internal_reg[i] <= 32'h0000_0000;
+        end
+
+    end
+
+    else begin
+
+        if (reg_write && rd_address != 5'b00000) begin
+            internal_reg[rd_address] <= write_data;
+        end
+        
+    end
+
+ end
+
+
+ endmodule
